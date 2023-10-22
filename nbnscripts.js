29314ac09710432b8b15d525f803f39cdb61f7d2
hqi5 <144130272+hqi5@users.noreply.github.com>
2023-10-22 03:26:28 -0400

    Update nbnscripts.js

--- nbnscripts.js
+++ nbnscripts.js
@@ -44,7 +44,7 @@ const nbdatabase = {
 }
 
 /**
- * gets the user id
+ * creates a database structure from the group
  */
 function databaseFromGroup(){
     var groupsFetch = JSON.parse(UrlFetchApp.fetch("https://groups.roblox.com/v1/groups/2845412/roles").getContentText());
@@ -74,11 +74,12 @@ function databaseFromGroup(){
                 // fetches the username for the specific object
                 usernameGet = ("username" in allData.data[j] ? (allData.data[j]['username']) : '');
                 userIdGet = ("userId" in allData.data[j] ? (allData.data[j]['userId']) : '');
+                
                 // creates a user object and pushes it onto the sample database
                 if (username != undefined){
                     const user = {
                         username: usernameGet,
-                        userId: userIdGet,       // TO BE IMPLEMENTED TBI
+                        userId: userIdGet,
                         rank: rank
                     }
                     databaseGroup.push(user)
@@ -98,10 +99,77 @@ function databaseFromGroup(){
     return databaseGroup
 }
 
+/**
+ * create a database structure from the sheet
+ */
+function databaseFromSheet(){
+    var databaseSheet = []
+    var main = SpreadsheetApp.getActive().getSheetByName("Main");
+    main.activate();
+    var last = main.getLastRow()
+    var values = main.getDataRange().getValues()
+    for(var i = 9; i < last; i++){
+        var username = values[i][1]
+        var userID = values[i][2]
+        var rank = values[i][3]
+        var l1 = values[i][6]
+        var l2 = values[i][8]
+        var l3 = values[i][10]
+        var m1 = values[i][13]
+        var m2 = values[i][16]
+        var m3 = values[i][19]
+        var h1 = values[i][22]
+        var h2 = values[i][25]
+        var h3 = values[i][28]
+
+        const user = {
+            username: username,
+            userId: userID,
+            rank: rank,
+            l1: l1,
+            l2: l2,
+            l3: l3,
+            m1: m1,
+            m2: m2,
+            m3: m3,
+            h1: h1,
+            h2: h2,
+            h3: h3,
+        }
+        databaseSheet.push(user)
+    }
+    return databaseSheet
+}
+
+/**
+ * compares 2 databases and returns entires that are different
+ * d1 should be base database (sheet), d2 should be new database (group)
+ */
+function compareDatabases(d1, d2){
+    var databaseSheet = []
+    
+    // double for loop to search
+    for(i in d1){
+        for(j in d2){
+            //if it equals name, then match
+            if(d1[i].username == d2[j].username){
+                Logger.log(d1[i].rank, d2[j].rank)
+                if(d1[i].rank != d2[j].rank){
+                    databaseSheet.push(d2[j])
+                }
+            }
+        }
+    }
+    return databaseSheet
+}
+
 function meow(){
-    var list1 = databaseFromGroup()
-    for(i in list1){
-        Logger.log(list1[i])
+    var list1 = databaseFromSheet()
+    var list2 = databaseFromGroup()
+    var compare = compareDatabases(list1, list2)
+    for(i in compare){
+        Logger.log(compare[i])
+        
     }
     
 }
